baeb0dd86f3acbce11f03c8ef7fa96a1586b076c
Dominik Suszek <dominik.suszek@gmail.com>
2024-05-18 17:18:22 +0200

    Basic structure of pipelines added

--- asi-grupa-6/src/asi_grupa_6/pipeline_registry.py
+++ asi-grupa-6/src/asi_grupa_6/pipeline_registry.py
@@ -1,16 +1,31 @@
 """Project pipelines."""
+from __future__ import annotations
 from typing import Dict
-
 from kedro.framework.project import find_pipelines
 from kedro.pipeline import Pipeline
 
+from pipelines import (
+    data_processing as dp,
+    data_science as ds,
+    model_evaluation as me
+)
+
 
 def register_pipelines() -> Dict[str, Pipeline]:
-    """Register the project's pipelines.
+    """
+    Registers the project's pipelines.
 
     Returns:
         A mapping from pipeline names to ``Pipeline`` objects.
     """
-    pipelines = find_pipelines()
-    pipelines["__default__"] = sum(pipelines.values())
-    return pipelines
+    data_processing_pipeline = dp.create_pipeline()
+    data_science_pipeline = ds.create_pipeline()
+    model_evaluation_pipeline = me.create_pipeline()
+
+
+    return {
+        "dp": data_processing_pipeline,
+        "ds": data_science_pipeline,
+        "me": model_evaluation_pipeline,
+        "__default__": data_processing_pipeline + data_science_pipeline + model_evaluation_pipeline
+    }
